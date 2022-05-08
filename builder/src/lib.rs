651578376078978230b72259dc8c37edb6da0032
mm1995tk <jimi.woodstock1995@gmail.com>
2022-05-08 16:14:44 +0900

    builder/04

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -20,21 +20,41 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
 
         impl #bident {
-            fn executable(&mut self, executable: String) -> &mut Self {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
                 self.executable = Some(executable);
                 self
             }
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
                 self.args = Some(args);
                 self
             }
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
                 self.env = Some(env);
                 self
             }
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
                 self.current_dir = Some(current_dir);
                 self
+            }
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                if self.executable.is_none() {
+                    return Err("executable is not set".into())
+                }
+                if self.args.is_none() {
+                    return Err("args is not set".into())
+                }
+                if self.env.is_none() {
+                    return Err("env is not set".into())
+                }
+                if self.current_dir.is_none() {
+                    return Err("current_dir is not set".into())
+                }
+                Ok(#name {
+                    executable: self.executable.clone().ok_or("executable is not set")?,
+                    args: self.args.clone().ok_or("args is not set")?,
+                    env: self.env.clone().ok_or("env is not set")?,
+                    current_dir: self.current_dir.clone().ok_or("current_dir is not set")?,
+                })
             }
         }
 
